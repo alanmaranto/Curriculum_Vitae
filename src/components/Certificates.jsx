fcf1d05ac466f6eb9c5156c2805be23856436996
alan <alanarostegui@hotmail.com>
2019-06-14 11:23:58 -0500

    Portafolio

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Certificates = props => (
+    <div className="Certificates">
+        <div className="Certificates-container">
+
+        {
+                props.data.map((cer, index) => (
+                    <div className="Certificates-item" key={`Cer-${index}`}>
+                    <h3>{cer.name} @ {cer.institution} 
+                        <span> {cer.date} </span>
+                    </h3>
+                    <p>{cer.description}</p>
+                    </div>
+                ))
+        }
+
+        </div>
+    </div>
+);
+
+export default Certificates;
